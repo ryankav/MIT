229bea5856788acfb34cffb9fbec928cf916ae13
ryan_kav <ryan.p.kav@gmail.com>
2020-02-10 09:10:03 +0000

    Uploading Completed problem set 4
    Uploading a completed version of problem set 4. Have been unable to run the data sets and get an output image as I am running Python with 64 bits where as the PIL library runs on 32 bits. Could make changes and get the output. However the code I've written has passed the initial tests and believe I've gained what I need to regarding hashing from this problem set already.

--- 6.006/PS4/dist/dnaseq.py
+++ 6.006/PS4/dist/dnaseq.py
@@ -10,37 +10,102 @@ class Multidict:
     # Initializes a new multi-value dictionary, and adds any key-value
     # 2-tuples in the iterable sequence pairs to the data structure.
     def __init__(self, pairs=[]):
-        raise Exception("Not implemented!")
+        
+        self.dict = {}
+        
+        for k_v in pairs:
+            self.put(k_v[0],k_v[1])
     # Associates the value v with the key k.
     def put(self, k, v):
-        raise Exception("Not implemented!")
+        
+        value=self.dict.get(k,None)
+        if not value:
+            self.dict[k]=[v]
+        else:
+            value.append(v)
     # Gets any values that have been associated with the key k; or, if
     # none have been, returns an empty sequence.
     def get(self, k):
-        raise Exception("Not implemented!")
+        
+        value = self.dict.get(k,[])
+        
+        return value
 
 # Given a sequence of nucleotides, return all k-length subsequences
 # and their hashes.  (What else do you need to know about each
 # subsequence?)
 def subsequenceHashes(seq, k):
-    raise Exception("Not implemented!")
-
+    try:
+        subseq = ''
+        pos=0
+        for i in range(k):
+            subseq+=next(seq)
+        rol_hash=RollingHash(subseq)
+        
+        while True:
+            yield rol_hash.current_hash(),(subseq,pos)
+            
+            prev_char=subseq[0]
+            subseq=subseq[1:]+next(seq)
+            rol_hash.slide(prev_char,subseq[-1])
+            
+            pos+=1
+            
+    except StopIteration:
+        return
+        
 # Similar to subsequenceHashes(), but returns one k-length subsequence
 # every m nucleotides.  (This will be useful when you try to use two
 # whole data files.)
 def intervalSubsequenceHashes(seq, k, m):
-    raise Exception("Not implemented!")
-
+    try:
+        subseq = ''
+        pos=0
+        for i in range(k):
+            subseq+=next(seq)
+        rol_hash=RollingHash(subseq)
+        
+        while True:
+            yield rol_hash.current_hash(),(subseq,pos)
+            
+            #this may not be the fastest approach but is deffinetely the easiest
+            #to implement.
+            for i in range(m):
+                prev_char=subseq[0]
+                subseq=subseq[1:]+next(seq)
+                rol_hash.slide(prev_char,subseq[-1])
+                
+                pos+=1
+    except StopIteration:
+        return
 # Searches for commonalities between sequences a and b by comparing
 # subsequences of length k.  The sequences a and b should be iterators
 # that return nucleotides.  The table is built by computing one hash
 # every m nucleotides (for m >= k).
-def getExactSubmatches(a, b, k, m):
-    raise Exception("Not implemented!")
 
-if __name__ == '__main__':
+def getExactSubmatches(a, b, k, m):
+    
+    dictionary = Multidict(intervalSubsequenceHashes(a,k,m))
+        
+    for key,(b_seq,b_pos) in subsequenceHashes(b,k):
+        seq_and_pos = dictionary.get(key)
+        if not seq_and_pos:
+            continue
+        matching_pos=[a_pos for a_seq,a_pos in seq_and_pos if a_seq==b_seq]
+        
+        if not matching_pos:
+            continue
+        
+        for a_pos in matching_pos:
+            yield a_pos, b_pos
+        
+        
+        
+if __name__=='__main__':    
+
+        
     if len(sys.argv) != 4:
-        print 'Usage: {0} [file_a.fa] [file_b.fa] [output.png]'.format(sys.argv[0])
+        print('Usage: {0} [file_a.fa] [file_b.fa] [output.png]'.format(sys.argv[0]))
         sys.exit(1)
 
     # The arguments are, in order: 1) Your getExactSubmatches
